6237ec6d8fcae35c27aec8eb01bf6573da94b6e7
Melvin Cheng <87087846+melvincheng0830@users.noreply.github.com>
2021-09-17 16:32:46 +0800

    Update Tutorial 1 Sample.py

--- Sample Codes/Tutorial 1 Sample.py
+++ Sample Codes/Tutorial 1 Sample.py
@@ -1,17 +1,17 @@
 def function1():
-    print('This text represent the content of function 1')  #To be edited by member 1, Replace this line with your actual fucntion code
+    print('This text represent the content of function 1')  #To be edited by member 1, Replace this line with your actual function code
     
 
 def function2():
-    print('This text represent the content of function 2')  #To be edited by member 2, Replace this line with your actual fucntion code
+    print('This text represent the content of function 2')  #To be edited by member 2, Replace this line with your actual function code
 
 
 def function3():
-    print('This text represent the content of function 3')  #To be edited by member 3, Replace this line with your actual fucntion code
+    print('This text represent the content of function 3')  #To be edited by member 3, Replace this line with your actual function code
 
 
 def function4():
-    print('This text represent the content of function 4')  #To be edited by member 4, Replace this line with your actual fucntion code
+    print('This text represent the content of function 4')  #To be edited by member 4, Replace this line with your actual function code
 
 
 #The Main function edited by Group leader
